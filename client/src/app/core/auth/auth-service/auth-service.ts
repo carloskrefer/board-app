c6605ee6314ea33a861d27cda313f3972c7ec137
Carlos Eduardo Krefer <carloskrefer@hotmail.com>
2026-08-09 17:32:54 -0300

    Adds auth service

---
+++ client/src/app/core/auth/auth-service/auth-service.ts
@@ -0,0 +1,16 @@
+import { inject, Service } from '@angular/core';
+import { environment } from '../../../../../environments/environment';
+import { LogInRequest, LogInResponse } from './dtos/login-dtos';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Service()
+export class AuthService {
+    readonly url = `${environment.api.board.url}/api/users`;
+
+    http = inject(HttpClient);
+
+    login(request: LogInRequest): Observable<LogInResponse> {
+        return this.http.post<LogInResponse>(`${this.url}/login`, request);
+    }
+}
