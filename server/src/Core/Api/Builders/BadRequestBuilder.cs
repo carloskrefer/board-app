a95bffd7ce1764828bf8fdbbf72df1c1cfd2de95
Carlos Eduardo Krefer <carloskrefer@hotmail.com>
2026-08-10 14:30:50 -0300

    Standardizes badrequest body to ApiProblemDetails

--- server/src/Core/Api/Builders/BadRequestBuilder.cs
+++ server/src/Core/Api/Builders/BadRequestBuilder.cs
@@ -7,8 +7,10 @@ namespace Core.Api.Builders;
 
 public static class BadRequestConstants
 {
-    public const string DefaultTitle = "One or more validation errors occurred.";
-    public const string DefaultDetail = "See the errors property for details.";
+    public const string TypeDefault = "https://tools.ietf.org/html/rfc9110#section-15.5.1";
+    public const string TitleGeneral = "Bad Request";
+    public const string TitleDetailed = "One or more validation errors occurred.";
+    public const string DetailDetailed = "See the errors property for details.";
 }
 
 public class BadRequestBuilder
@@ -29,36 +31,31 @@ public class BadRequestBuilder
 
     public IActionResult Build()
     {
-        if (!_errors.Any())
-            return _controller.BadRequest();
-
-        string title;
-        string detail;
-        DetailedResponseError[] errors = [];
-
-        if (HasSingleErrorAndIsNotDetailed())
+        var body = new ApiProblemDetails
         {
-            title = _errors[0].Code;
-            detail = _errors[0].Message;
-        }
-        else
-        {
-            title = BadRequestConstants.DefaultTitle;
-            detail = BadRequestConstants.DefaultDetail;
-            errors = _errors.Select(e => e as DetailedResponseError ?? new DetailedResponseError(e)).ToArray();
-        }
-
-        var response = new ApiProblemDetails
-        {
-            Title = title,
+            Type = BadRequestConstants.TypeDefault,
+            Title = BadRequestConstants.TitleGeneral,
             Status = StatusCodes.Status400BadRequest,
-            Detail = detail,
             Instance = _controller.HttpContext.Request.Path,
-            Errors = errors,
             TraceId = Activity.Current?.TraceId.ToString()
         };
 
-        return _controller.BadRequest(response);
+        if (!_errors.Any())
+            return _controller.BadRequest(body);
+
+        if (HasSingleErrorAndIsNotDetailed())
+        {
+            body.Title = _errors[0].Code;
+            body.Detail = _errors[0].Message;
+        }
+        else
+        {
+            body.Title = BadRequestConstants.TitleDetailed;
+            body.Detail = BadRequestConstants.DetailDetailed;
+            body.Errors = _errors.Select(e => e as DetailedResponseError ?? new DetailedResponseError(e)).ToArray();
+        }
+
+        return _controller.BadRequest(body);
     }
 
     private bool HasSingleErrorAndIsNotDetailed() => 
