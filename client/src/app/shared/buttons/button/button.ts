df2ba32d9a4d4d4e45381b51cb4ea32f7b3f3dd3
Carlos Eduardo Krefer <carloskrefer@hotmail.com>
2026-08-21 19:09:50 -0300

    Adds more functionalities to button component
    - New icon configuration
    - New color configurations
    - New content alignment configuration (usefull when the button's width is not fit-content, so you can align it to the right, left or center)
    - New horizontal padding configuration
    - New width configuration
    - New hover effect configuration

--- client/src/app/shared/buttons/button/button.ts
+++ client/src/app/shared/buttons/button/button.ts
@@ -1,26 +1,37 @@
 import { Component, computed, input } from '@angular/core';
 import { Spinner } from '../../symbols/spinner/spinner';
+import { IconType } from '../../symbols/icon/icon-type';
+import { Icon } from '../../symbols/icon/icon';
 
 @Component({
   selector: 'app-button',
-  imports: [Spinner],
+  imports: [Spinner, Icon],
   templateUrl: './button.html',
   styleUrl: './button.scss',
 })
 export class Button {
     label = input.required<string>();
     type = input<'button' | 'submit'>('button');
-    mainColor = input<'primary' | 'secondary'>('primary');
+    mainColor = input<'primary' | 'secondary' | 'error' | 'gray-medium' | 'gray-light'>('primary');
     mode = input<'fill' | 'outline'>('fill');
     size = input<'small' | 'medium' | 'large'>('medium');
+    icon = input<IconType>();
     borderHidden = input<boolean>(false);
     isLoading = input<boolean>(false);
+    widthType = input<'auto-width' | 'full-width'>('full-width');
+    hoverEffect = input<'underline' | 'background-color'>('background-color');
+    alignment = input<'start' | 'center' | 'end'>('center');
+    paddingHorizontal = input<'none' | 'small'>('small');
 
     classes = computed(() => ([
         `${this.mainColor()}-main-color`,
         this.mode(),
         this.size(),
         this.borderHidden() ? 'border-hidden' : '',
+        this.widthType(),
+        `hover-${this.hoverEffect()}`,
+        `justify-content-${this.alignment()}`,
+        `padding-horizontal-${this.paddingHorizontal()}`
     ]));
 
     spinnerColor = computed(() => {
@@ -30,6 +41,9 @@ export class Button {
         if (mode == 'fill')
             return color === 'primary' ? 'on-primary' : 'on-secondary';
 
-        return color;
+        if (color === 'primary' || color === 'secondary' || color === 'error')
+            return color;
+
+        return 'primary';
     });
 }
